977c2ae2df46ba35ee01e0e72ba7dc4626fdd6b7
WatanabeChika <wanakachi@foxmail.com>
2025-12-13 16:17:40 +0800

    add github link into homepage

--- src/Homepage.vue
+++ src/Homepage.vue
@@ -2,8 +2,11 @@
   <div class="homepage">
     <img src="/images/logo.png" alt="Logo" class="logo">
     <div class="description">
-      <h1>这里是Wanakachi的工具小站</h1>
+      <h1>这里是 Wanakachi 的工具小站</h1>
       <p>这里集合了各种实用 (?) 工具，请通过上方导航栏进行选择。</p>
+      <p>本项目的 Github 链接：
+        <a href="https://github.com/WatanabeChika/Wana_tools">Wana_tools</a>
+      </p>
       <p>我的博客：
         <a href="https://watanabechika.github.io/Wana_blog/">Wana_blog</a>
       </p>
